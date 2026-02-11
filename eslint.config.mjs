d010c350606202b814e6c64747c777db71bf178a
Kint <kborbano@zrc.pshs.edu.ph>
2026-02-11 21:48:45 +0800

    chore: added husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,4 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     ignores: [
       'node_modules/**',
@@ -22,23 +10,8 @@ const eslintConfig = [
     ],
   },
   {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-    },
-  },
-  {
     files: ['components/ModelViewer.tsx'],
     rules: {
-      '@next/next/no-img-element': 'off',
-      'jsx-a11y/alt-text': 'off',
       'react-hooks/rules-of-hooks': 'off',
       'react-hooks/exhaustive-deps': 'off',
     },
